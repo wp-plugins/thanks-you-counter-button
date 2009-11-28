1b1b96194227ec88761b6ec41b3caca9d03f7c09
shinephp <shinephp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-28 09:23:13 +0000

    v.1.4.1 update
    git-svn-id: https://plugins.svn.wordpress.org/thanks-you-counter-button/trunk@177656 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- thankyou.php
+++ thankyou.php
@@ -3,7 +3,7 @@
 Plugin Name: Thank You Counter Button
 Plugin URI: http://www.shinephp.com/thank-you-counter-button-wordpress-plugin/
 Description: Every time a new visitor clicks the "Thank you" button, one point is added to the total "thanks" counter for this post.
-Version: 1.4
+Version: 1.4.1
 Author: Vladimir Garagulya
 Author URI: http://www.shinephp.com
 Text Domain: thankyou
